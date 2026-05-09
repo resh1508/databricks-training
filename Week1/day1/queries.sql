672684af120c80f72cebf45bf1966742a08a0012
resh1508 <23pa1a4217@vishnu.edu.in>
2026-05-09 17:55:11 +0530

    Update queries.sql

--- Week1/day1/queries.sql
+++ Week1/day1/queries.sql
@@ -1,1 +1,11 @@
+Basic Queries (5)
 
+1. Select all columns from the Employee table.
+
+2. Select only the name and salary columns from the Employee table.
+
+3. Select employees who are older than 30.
+
+4. Select the names of all departments.
+
+5. Select employees who work in the IT department.
