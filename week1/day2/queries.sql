9c163941c325613e9a0c3491af5599ee53d9bd95
resh1508 <23pa1a4217@vishnu.edu.in>
2026-05-18 13:50:11 +0530

    Update queries.sql

--- week1/day2/queries.sql
+++ week1/day2/queries.sql
@@ -48,5 +48,60 @@ WHERE salary = 52000;
 --GROUP BY
 
 --11.Find total salary department-wise.
+SELECT sum(salary),department
+FROM Employees
+GROUP BY department;
 
- 
+--12.Find average salary in each department.
+SELECT AVG(salary),department
+FROM Employees
+GROUP BY department;
+
+--13.Count employees in each city.
+SELECT COUNT(emp_id),city
+FROM Employees
+GROUP BY city;
+
+--14.Find maximum salary in each department.
+SELECT MAX(salary),department
+FROM Employees
+GROUP BY department;
+
+--15.Find minimum experience department-wise.
+SELECT MIN(salary),department
+FROM Employees
+GROUP BY department;
+
+--HAVING
+
+--16.Find departments having more than 3 employees.
+SELECT COUNT(emp_id),department
+FROM Employees
+GROUP BY department
+HAVING COUNT(emp_id) > 3;
+
+--17.Find departments where average salary is greater than 60000.
+SELECT AVG(salary),department
+FROM Employees
+GROUP BY department
+HAVING AVG(salary) > 60000;
+
+--18.Find cities having more than 2 employees.
+SELECT COUNT(emp_id),city
+FROM Employees
+GROUP BY city
+HAVING COUNT(emp_id) > 2;
+
+--19.Find departments where total salary is greater than 200000.
+SELECT SUM(salary),department
+FROM Employees
+GROUP BY department
+HAVING SUM(salary) > 200000;
+
+--20.Find departments where maximum salary is above 90000.
+SELECT MAX(salary),department
+FROM Employees
+GROUP BY department
+HAVING MAX(salary) > 90000;
+
+--TOP
